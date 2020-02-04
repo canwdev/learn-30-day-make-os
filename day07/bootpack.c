d5eff464cb0fb398f74f7c0d8624fb657fed78f4
canwdev <c2097@live.com>
2020-02-04 17:05:08 +0800

    day07 - harib04d

--- day07/bootpack.c
+++ day07/bootpack.c
@@ -1,15 +1,20 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+extern struct KEYBUF keybuf;
+
 void HariMain(void)
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
   char s[40], mcursor[252];
-  int mx, my;
+  int mx, my, i, j;
 
   init_gdtidt();
   init_pic();
   io_sti(); /* IDT/PIC的初始化已经完成，于是开放CPU的中断 */
+
+	io_out8(PIC0_IMR, 0xf9); /* 开放PIC1和键盘中断(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* 开放鼠标中断(11101111) */
 
   init_palette(); // 初始化调色板
   init_screen8(binfo->vram, binfo->scrnx, binfo->scrny); // 绘制背景
@@ -24,12 +29,27 @@ void HariMain(void)
   // 输出debug信息
   sprintf(s, "(%d, %d)", mx, my);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-
-	io_out8(PIC0_IMR, 0xf9); /* 开放PIC1和键盘中断(11111001) */
-	io_out8(PIC1_IMR, 0xef); /* 开放鼠标中断(11101111) */
   
   for (;;)
   {
-    io_hlt();
+    io_cli(); // 屏蔽中断
+
+    if (keybuf.len == 0)
+    {
+      io_stihlt();
+    }
+    else
+    {
+      i = keybuf.data[keybuf.next_r];
+      keybuf.len--;
+      keybuf.next_r++;
+      if (keybuf.next_r == 32) {
+        keybuf.next_r = 0;
+      }
+      io_sti(); // 开放中断
+      sprintf(s, "%02X", i);
+      boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 16, 15, 31);
+      putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+    }
   }
 }
--- day07/bootpack.h
+++ day07/bootpack.h
@@ -14,6 +14,8 @@ struct BOOTINFO { /* 0x0ff0-0x0fff */
 void io_hlt(void);
 void io_cli(void);
 void io_sti(void);
+void io_stihlt(void);
+int io_in8(int port);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
@@ -80,6 +82,11 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define AR_INTGATE32	0x008e
 
 /* int.c */
+struct KEYBUF
+{
+	unsigned char data[32];
+	int next_r, next_w, len;
+};
 void init_pic(void);
 void inthandler21(int *esp);
 void inthandler27(int *esp);
