a533a6e21a12325210d9df57df76401fc6d04a25
canwdev <c2097@live.com>
2020-02-06 10:48:07 +0800

    day08 - harib05c

--- day08/bootpack.c
+++ day08/bootpack.c
@@ -3,6 +3,7 @@
 
 struct MOUSE_DEC {
   unsigned char buf[3], phase;
+  int x, y, btn;
 };
 
 extern struct FIFO8 keyfifo, mousefifo;
@@ -71,8 +72,23 @@ void HariMain(void)
 
         if (mouse_decode(&mdec, i) != 0) {
           // 鼠标的3个字节都齐了，显示出来
-          sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-          boxfill8(binfo->vram, binfo->scrnx, COL8_BG, 32, 16, 32+8*8-1, 31);
+
+          sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+
+          if ((mdec.btn & 0x01) != 0)
+          {
+            s[1] = 'L';
+          }
+          if ((mdec.btn & 0x02) != 0)
+          {
+            s[3] = 'R';
+          }
+          if ((mdec.btn & 0x04) != 0)
+          {
+            s[2] = 'C';
+          }
+
+          boxfill8(binfo->vram, binfo->scrnx, COL8_BG, 32, 16, 32 + 15 * 8 - 1, 31);
           putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
         } 
 
@@ -142,14 +158,18 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
     }
     return 0;
   }
-  else if (mdec->phase == 1)
+  if (mdec->phase == 1)
   {
     // 等待鼠标的第1字节
-    mdec->buf[0] = dat;
-    mdec->phase = 2;
+    if ((dat & 0xc8) == 0x08)
+    {
+      // 如果第一字节正确
+      mdec->buf[0] = dat;
+      mdec->phase = 2;
+    }
     return 0;
   }
-  else if (mdec->phase == 2)
+  if (mdec->phase == 2)
   {
     // 等待鼠标的第2字节
     mdec->buf[1] = dat;
@@ -161,6 +181,18 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
     // 等待鼠标的第3字节
     mdec->buf[2] = dat;
     mdec->phase = 1;
+    mdec->btn = mdec->buf[0] & 0x07;
+    mdec->x = mdec->buf[1];
+    mdec->y = mdec->buf[2];
+    if ((mdec->buf[0] & 0x10) != 0)
+    {
+      mdec->x |= 0xffffff00;
+    }
+    if ((mdec->buf[0] & 0x20) != 0)
+    {
+      mdec->y |= 0xffffff00;
+    }
+    mdec->y = - mdec->y; // 鼠标的y方向与画面符号相反
     return 1;
   }
   return -1; // 应该不可能到这里来
